a0acad38448deb6a5e53e8d56370f6c1e9ca1134
Omer Hamerman <omerxx@gmail.com>
2024-05-05 22:38:05 +0100

    customizing popups

--- scripts/floax.sh
+++ scripts/floax.sh
@@ -11,12 +11,12 @@ else
     # Check if the session 'scratch' exists
     if tmux has-session -t scratch 2>/dev/null; then
         # Popup that attaches to existing 'scratch' session
-        tmux popup -E -w "$FLOAX_WIDTH" -h "$FLOAX_HEIGHT" "tmux attach-session -t scratch"
+        tmux popup -S fg=magenta -s fg=blue,bold -T "FloaX" -b rounded -E -w "$FLOAX_WIDTH" -h "$FLOAX_HEIGHT" "tmux attach-session -t scratch"
     else
         # Create a new session named 'scratch' and attach to it
         tmux new-session -d -c "$(tmux display-message -p '#{pane_current_path}')" -s scratch
         tmux set-option -t scratch status off
-        tmux popup -E -w "$FLOAX_WIDTH" -h "$FLOAX_HEIGHT" "tmux attach-session -t scratch"
+        tmux popup -S fg=magenta -E -w "$FLOAX_WIDTH" -h "$FLOAX_HEIGHT" "tmux attach-session -t scratch"
         # tmux attach-session -t scratch
     fi
 fi
