4ee8ce4979e4f92424a78a2c076065b69c78995e
Omer Hamerman <omerxx@gmail.com>
2024-05-06 14:26:58 +0100

    additional color options

--- floax.tmux
+++ floax.tmux
@@ -16,4 +16,9 @@ tmux bind-key "$(tmux_option_or_fallback "@floax-bind-menu" "P")" run-shell "$CU
 tmux setenv -g FLOAX_WIDTH "$(tmux_option_or_fallback '@floax-width' '80%')" 
 tmux setenv -g FLOAX_HEIGHT "$(tmux_option_or_fallback '@floax-height' '80%')" 
 
+# Options: black, red, green, yellow, blue, magenta, cyan, white
+tmux setenv -g FLOAX_BORDER_COLOR "$(tmux_option_or_fallback '@floax-border-color' 'magenta')" 
+tmux setenv -g FLOAX_TEXT_COLOR "$(tmux_option_or_fallback '@floax-text-color' 'blue')" 
+tmux setenv -g FLOAX_TITLE "$(tmux_option_or_fallback '@floax-title' 'FloaX')" 
+
 eval "$(tmux showenv -s)"
--- scripts/floax.sh
+++ scripts/floax.sh
@@ -1,8 +1,22 @@
 #!/usr/bin/env bash
 
 
-FLOAX_WIDTH=$(tmux showenv -g FLOAX_WIDTH | cut -d '=' -f 2)
-FLOAX_HEIGHT=$(tmux showenv -g FLOAX_HEIGHT | cut -d '=' -f 2)
+popup () {
+        tmux popup \
+            -S fg="$FLOAX_BORDER_COLOR" \
+            -s fg="$FLOAX_TEXT_COLOR" \
+            -T "$FLOAX_TITLE" \
+            -b rounded -E \
+            -w "$FLOAX_WIDTH" \
+            -h "$FLOAX_HEIGHT" \
+            "tmux attach-session -t scratch"
+}
+
+FLOAX_WIDTH=$(envvar_value FLOAX_WIDTH)
+FLOAX_HEIGHT=$(envvar_value FLOAX_HEIGHT)
+FLOAX_BORDER_COLOR=$(envvar_value FLOAX_BORDER_COLOR)
+FLOAX_TEXT_COLOR=$(envvar_value FLOAX_TEXT_COLOR)
+FLOAX_TITLE=$(envvar_value FLOAX_TITLE)
 
 if [ "$(tmux display-message -p '#{session_name}')" = "scratch" ]; then
     # Detach the client
@@ -11,12 +25,11 @@ else
     # Check if the session 'scratch' exists
     if tmux has-session -t scratch 2>/dev/null; then
         # Popup that attaches to existing 'scratch' session
-        tmux popup -S fg=magenta -s fg=blue,bold -T "FloaX" -b rounded -E -w "$FLOAX_WIDTH" -h "$FLOAX_HEIGHT" "tmux attach-session -t scratch"
+        popup
     else
         # Create a new session named 'scratch' and attach to it
         tmux new-session -d -c "$(tmux display-message -p '#{pane_current_path}')" -s scratch
         tmux set-option -t scratch status off
-        tmux popup -S fg=magenta -E -w "$FLOAX_WIDTH" -h "$FLOAX_HEIGHT" "tmux attach-session -t scratch"
-        # tmux attach-session -t scratch
+        popup
     fi
 fi
