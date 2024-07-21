d91e7d3466891ba8364eeb6260a03f0a161859ef
Mikko Haapanen <26046991+haapmik@users.noreply.github.com>
2024-07-22 01:30:25 +0300

    fix: re-added small screen fix without infinite loop

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -44,40 +44,6 @@ unset_bindings() {
     tmux unbind -n C-M-u 
 }
 
-tmux_popup() {
-    FLOAX_WIDTH=$(envvar_value FLOAX_WIDTH)
-    if [ "$FLOAX_WIDTH" = "" ]; then
-        FLOAX_WIDTH="$(tmux_option_or_fallback '@floax-width' '80%')" 
-        tmux setenv -g FLOAX_WIDTH "$FLOAX_WIDTH" 
-    fi
-
-    FLOAX_HEIGHT=$(envvar_value FLOAX_HEIGHT)
-    if [ "$FLOAX_HEIGHT" = "" ]; then
-        FLOAX_HEIGHT="$(tmux_option_or_fallback '@floax-height' '80%')" 
-        tmux setenv -g FLOAX_HEIGHT "$FLOAX_HEIGHT" 
-    fi
-
-    FLOAX_TITLE=$(envvar_value FLOAX_TITLE)
-    if [ -z "$FLOAX_TITLE" ]; then
-        FLOAX_TITLE="$DEFAULT_TITLE"
-    fi
-
-    FLOAX_SESSION_NAME=$(envvar_value FLOAX_SESSION_NAME)
-    if [ -z "$FLOAX_SESSION_NAME" ]; then
-        FLOAX_SESSION_NAME="$DEFAULT_SESSION_NAME"
-    fi
-
-
-    # TODO: make this optional:
-    current_dir=$(tmux display -p '#{pane_current_path}')
-    scratch_path=$(tmux display -t scratch -p '#{pane_current_path}')
-    if [ "$scratch_path" != "$current_dir" ] && [ "$FLOAX_CHANGE_PATH" = "true" ]; then
-        tmux send-keys -R -t "$FLOAX_SESSION_NAME" " cd $current_dir" C-m
-    fi
-
-    pop
-}
-
 tmux_version() {
   tmux -V | cut -d ' ' -f 2
 }
@@ -99,20 +65,48 @@ is_tmux_version_supported() {
     return 1
 }
 
-pop() {
+tmux_popup() {
+    # TODO: make this optional:
+    current_dir=$(tmux display -p '#{pane_current_path}')
+    scratch_path=$(tmux display -t scratch -p '#{pane_current_path}')
+    if [ "$scratch_path" != "$current_dir" ] && [ "$FLOAX_CHANGE_PATH" = "true" ]; then
+        tmux send-keys -R -t scratch "cd $current_dir" C-m
+    fi
+
     if is_tmux_version_supported; then
-        tmux popup \
-            -S fg="$FLOAX_BORDER_COLOR" \
-            -s fg="$FLOAX_TEXT_COLOR" \
-            -T "$FLOAX_TITLE" \
-            -w "$FLOAX_WIDTH" \
-            -h "$FLOAX_HEIGHT" \
-            -b rounded \
-            -E \
-            "tmux attach-session -t \"$FLOAX_SESSION_NAME\"" 
+        if ! pop; then
+            tmux setenv -g FLOAX_WIDTH "$(tmux_option_or_fallback '@floax-width' '80%')" 
+            tmux setenv -g FLOAX_HEIGHT "$(tmux_option_or_fallback '@floax-height' '80%')"
+            pop
+        fi
     else
         tmux display-message \
             -d 2000 \
             "FloaX requires tmux version 3.3 or newer"
     fi
+}
+
+pop() {
+    FLOAX_WIDTH=$(envvar_value FLOAX_WIDTH)
+    FLOAX_HEIGHT=$(envvar_value FLOAX_HEIGHT)
+
+    FLOAX_TITLE=$(envvar_value FLOAX_TITLE)
+    if [ -z "$FLOAX_TITLE" ]; then
+        FLOAX_TITLE="$DEFAULT_TITLE"
+    fi
+
+    FLOAX_SESSION_NAME=$(envvar_value FLOAX_SESSION_NAME)
+    if [ -z "$FLOAX_SESSION_NAME" ]; then
+        FLOAX_SESSION_NAME="$DEFAULT_SESSION_NAME"
+    fi
+
+    tmux popup \
+        -S fg="$FLOAX_BORDER_COLOR" \
+        -s fg="$FLOAX_TEXT_COLOR" \
+        -T "$FLOAX_TITLE" \
+        -w "$FLOAX_WIDTH" \
+        -h "$FLOAX_HEIGHT" \
+        -b rounded \
+        -E \
+        "tmux attach-session -t \"$FLOAX_SESSION_NAME\"" 
 }
